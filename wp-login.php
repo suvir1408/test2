db17a06f917d913765cfc93a462027fb709d52db
test2 <suvir1408@gmail,com>
2021-06-08 21:20:37 +0700

    complete 1

--- wp-login.php
+++ wp-login.php
@@ -1,108 +1,100 @@
 <!DOCTYPE html>
-<html lang="vi">
+<html lang="en-GB">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Đăng nhập &lsaquo; Suvir &#8212; WordPress</title>
+	<title>Log In &lsaquo; Suvir &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="https://suvir1408.github.io/huy1408/wp-includes/css/dashicons.min.css?ver=5.7.2" type="text/css" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://suvir1408.github.io/huy1408/wp-includes/css/buttons.min.css?ver=5.7.2" type="text/css" media="all">
-<link rel="stylesheet" id="forms-css" href="https://suvir1408.github.io/huy1408/wp-admin/css/forms.min.css?ver=5.7.2" type="text/css" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://suvir1408.github.io/huy1408/wp-admin/css/l10n.min.css?ver=5.7.2" type="text/css" media="all">
-<link rel="stylesheet" id="login-css" href="https://suvir1408.github.io/huy1408/wp-admin/css/login.min.css?ver=5.7.2" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://suvir1408.github.io/huy1408/wp-includes/css/dashicons.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://suvir1408.github.io/huy1408/wp-includes/css/buttons.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="forms-css" href="https://suvir1408.github.io/huy1408/wp-admin/css/forms.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://suvir1408.github.io/huy1408/wp-admin/css/l10n.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="login-css" href="https://suvir1408.github.io/huy1408/wp-admin/css/login.min.css?ver=5.7.2" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
-	<body class="login no-js login-action-login wp-core-ui  locale-vi">
+	<body class="login no-js login-action-login wp-core-ui  locale-en-gb">
 	<script type="text/javascript">
 		document.body.className = document.body.className.replace('no-js','js');
 	</script>
 		<div id="login">
-		<h1><a href="https://vi.wordpress.org/">Xây dựng bằng WordPress</a></h1>
+		<h1><a href="https://en-gb.wordpress.org/">Powered by WordPress</a></h1>
 	
 		<form name="loginform" id="loginform" action="https://suvir1408.github.io/huy1408/wp-login.php" method="post">
 			<p>
-				<label for="user_login">Tên người dùng hoặc Địa chỉ Email</label>
+				<label for="user_login">Username or Email Address</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
 			</p>
 
 			<div class="user-pass-wrap">
-				<label for="user_pass">Mật khẩu</label>
+				<label for="user_pass">Password</label>
 				<div class="wp-pwd">
 					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20">
-					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Hiện mật khẩu">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
 						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
 					</button>
 				</div>
 			</div>
-						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Tự động đăng nhập</label></p>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
-				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Đăng nhập">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
 									<input type="hidden" name="redirect_to" value="https://suvir1408.github.io/huy1408/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="https://suvir1408.github.io/huy1408/wp-login.php?action=lostpassword">Bạn quên mật khẩu?</a>
+								<a href="https://suvir1408.github.io/huy1408/wp-login.php?action=lostpassword">Lost your password?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="https://suvir1408.github.io/huy1408/">&larr; Quay lại Suvir</a>		</p>
+			<a href="https://suvir1408.github.io/huy1408/">&larr; Go to Suvir</a>		</p>
 			</div>
-	<script type="text/javascript" src="https://suvir1408.github.io/huy1408/wp-includes/js/jquery/jquery.min.js?ver=3.5.1" id="jquery-core-js"></script>
-<script type="text/javascript" src="https://suvir1408.github.io/huy1408/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
-<script type="text/javascript" id="zxcvbn-async-js-extra">
-/* <![CDATA[ */
+	<script src="https://suvir1408.github.io/huy1408/wp-includes/js/jquery/jquery.min.js?ver=3.5.1" id="jquery-core-js"></script>
+<script src="https://suvir1408.github.io/huy1408/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+<script id="zxcvbn-async-js-extra">
 var _zxcvbnSettings = {"src":"https:\/\/suvir1408.github.io\/huy1408\/wp-includes\/js\/zxcvbn.min.js"};
-/* ]]> */
 </script>
-<script type="text/javascript" src="https://suvir1408.github.io/huy1408/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script type="text/javascript" src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4" id="wp-polyfill-js"></script>
-<script type="text/javascript" id="wp-polyfill-js-after">
+<script src="https://suvir1408.github.io/huy1408/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4" id="wp-polyfill-js"></script>
+<script id="wp-polyfill-js-after">
 ( 'fetch' in window ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"><\/scr' + 'ipt>' );( document.contains ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"><\/scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"><\/scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"><\/scr' + 'ipt>' );( 'objectFit' in document.documentElement.style ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-object-fit.min.js?ver=2.3.4"><\/scr' + 'ipt>' );
 </script>
-<script type="text/javascript" src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/hooks.min.js?ver=50e23bed88bcb9e6e14023e9961698c1" id="wp-hooks-js"></script>
-<script type="text/javascript" src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/i18n.min.js?ver=db9a9a37da262883343e941c3731bc67" id="wp-i18n-js"></script>
-<script type="text/javascript" id="wp-i18n-js-after">
+<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/hooks.min.js?ver=50e23bed88bcb9e6e14023e9961698c1" id="wp-hooks-js"></script>
+<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/i18n.min.js?ver=db9a9a37da262883343e941c3731bc67" id="wp-i18n-js"></script>
+<script id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
-<script type="text/javascript" id="password-strength-meter-js-extra">
-/* <![CDATA[ */
-var pwsL10n = {"unknown":"Mật khẩu mạnh không xác định","short":"Rất yếu","bad":"Yếu","good":"Trung bình","strong":"Mạnh","mismatch":"Mật khẩu không khớp"};
-/* ]]> */
+<script id="password-strength-meter-js-extra">
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
 </script>
-<script type="text/javascript" id="password-strength-meter-js-translations">
+<script id="password-strength-meter-js-translations">
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
-} )( "default", {"translation-revision-date":"2021-05-13 14:44:15+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"vi_VN"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s đã ngừng hoạt động từ phiên bản %2$s! Sử dụng thay thế bằng %3$s."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
+} )( "default", {"translation-revision-date":"2021-03-05 12:20:18+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"en_GB"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
 </script>
-<script type="text/javascript" src="https://suvir1408.github.io/huy1408/wp-admin/js/password-strength-meter.min.js?ver=5.7.2" id="password-strength-meter-js"></script>
-<script type="text/javascript" src="https://suvir1408.github.io/huy1408/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
-<script type="text/javascript" id="wp-util-js-extra">
-/* <![CDATA[ */
+<script src="https://suvir1408.github.io/huy1408/wp-admin/js/password-strength-meter.min.js?ver=5.7.2" id="password-strength-meter-js"></script>
+<script src="https://suvir1408.github.io/huy1408/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
+<script id="wp-util-js-extra">
 var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-ajax.php"}};
-/* ]]> */
 </script>
-<script type="text/javascript" src="https://suvir1408.github.io/huy1408/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
-<script type="text/javascript" id="user-profile-js-extra">
-/* <![CDATA[ */
+<script src="https://suvir1408.github.io/huy1408/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
+<script id="user-profile-js-extra">
 var userProfileL10n = {"user_id":"0","nonce":"c035161ac3"};
-/* ]]> */
 </script>
-<script type="text/javascript" id="user-profile-js-translations">
+<script id="user-profile-js-translations">
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
-} )( "default", {"translation-revision-date":"2021-05-13 14:44:15+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"vi_VN"},"Your new password has not been saved.":["Mật khẩu mới của bạn chưa được lưu."],"Show":["Hiển thị"],"Hide":["Ẩn đi"],"Confirm use of weak password":["Chấp nhận sử dụng mật khẩu yếu."],"Hide password":["Ẩn mật khẩu"],"Show password":["Hiện mật khẩu"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
+} )( "default", {"translation-revision-date":"2021-03-05 12:20:18+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"en_GB"},"Your new password has not been saved.":["Your new password has not been saved."],"Show":["Show"],"Hide":["Hide"],"Confirm use of weak password":["Confirm use of weak password"],"Hide password":["Hide password"],"Show password":["Show password"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
 </script>
-<script type="text/javascript" src="https://suvir1408.github.io/huy1408/wp-admin/js/user-profile.min.js?ver=5.7.2" id="user-profile-js"></script>
+<script src="https://suvir1408.github.io/huy1408/wp-admin/js/user-profile.min.js?ver=5.7.2" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
