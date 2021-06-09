57276377fc011bb191e890a2eee4d4f7fedaa719
test2 <suvir1408@gmail.com>
2021-06-09 09:07:54 +0700

    test2

--- wp-login.php
+++ wp-login.php
@@ -5,11 +5,11 @@
 	<title>Log In &lsaquo; Suvir &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="https://suvir1408.github.io/huy1408/wp-includes/css/dashicons.min.css?ver=5.7.2" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://suvir1408.github.io/huy1408/wp-includes/css/buttons.min.css?ver=5.7.2" media="all">
-<link rel="stylesheet" id="forms-css" href="https://suvir1408.github.io/huy1408/wp-admin/css/forms.min.css?ver=5.7.2" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://suvir1408.github.io/huy1408/wp-admin/css/l10n.min.css?ver=5.7.2" media="all">
-<link rel="stylesheet" id="login-css" href="https://suvir1408.github.io/huy1408/wp-admin/css/login.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://suvir1408.github.io/test2/wp-includes/css/dashicons.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://suvir1408.github.io/test2/wp-includes/css/buttons.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="forms-css" href="https://suvir1408.github.io/test2/wp-admin/css/forms.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://suvir1408.github.io/test2/wp-admin/css/l10n.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="login-css" href="https://suvir1408.github.io/test2/wp-admin/css/login.min.css?ver=5.7.2" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
@@ -20,7 +20,7 @@
 		<div id="login">
 		<h1><a href="https://en-gb.wordpress.org/">Powered by WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="https://suvir1408.github.io/huy1408/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://suvir1408.github.io/test2/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Username or Email Address</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
@@ -38,33 +38,33 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
-									<input type="hidden" name="redirect_to" value="https://suvir1408.github.io/huy1408/wp-admin/">
+									<input type="hidden" name="redirect_to" value="https://suvir1408.github.io/test2/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="https://suvir1408.github.io/huy1408/wp-login.php?action=lostpassword">Lost your password?</a>
+								<a href="https://suvir1408.github.io/test2/wp-login.php?action=lostpassword">Lost your password?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="https://suvir1408.github.io/huy1408/">&larr; Go to Suvir</a>		</p>
+			<a href="https://suvir1408.github.io/test2/">&larr; Go to Suvir</a>		</p>
 			</div>
-	<script src="https://suvir1408.github.io/huy1408/wp-includes/js/jquery/jquery.min.js?ver=3.5.1" id="jquery-core-js"></script>
-<script src="https://suvir1408.github.io/huy1408/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+	<script src="https://suvir1408.github.io/test2/wp-includes/js/jquery/jquery.min.js?ver=3.5.1" id="jquery-core-js"></script>
+<script src="https://suvir1408.github.io/test2/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script id="zxcvbn-async-js-extra">
-var _zxcvbnSettings = {"src":"https:\/\/suvir1408.github.io\/huy1408\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/suvir1408.github.io\/test2\/wp-includes\/js\/zxcvbn.min.js"};
 </script>
-<script src="https://suvir1408.github.io/huy1408/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4" id="wp-polyfill-js"></script>
+<script src="https://suvir1408.github.io/test2/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="https://suvir1408.github.io/test2/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4" id="wp-polyfill-js"></script>
 <script id="wp-polyfill-js-after">
-( 'fetch' in window ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"><\/scr' + 'ipt>' );( document.contains ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"><\/scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"><\/scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"><\/scr' + 'ipt>' );( 'objectFit' in document.documentElement.style ) || document.write( '<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/vendor/wp-polyfill-object-fit.min.js?ver=2.3.4"><\/scr' + 'ipt>' );
+( 'fetch' in window ) || document.write( '<script src="https://suvir1408.github.io/test2/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"><\/scr' + 'ipt>' );( document.contains ) || document.write( '<script src="https://suvir1408.github.io/test2/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="https://suvir1408.github.io/test2/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="https://suvir1408.github.io/test2/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"><\/scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="https://suvir1408.github.io/test2/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"><\/scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="https://suvir1408.github.io/test2/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"><\/scr' + 'ipt>' );( 'objectFit' in document.documentElement.style ) || document.write( '<script src="https://suvir1408.github.io/test2/wp-includes/js/dist/vendor/wp-polyfill-object-fit.min.js?ver=2.3.4"><\/scr' + 'ipt>' );
 </script>
-<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/hooks.min.js?ver=50e23bed88bcb9e6e14023e9961698c1" id="wp-hooks-js"></script>
-<script src="https://suvir1408.github.io/huy1408/wp-includes/js/dist/i18n.min.js?ver=db9a9a37da262883343e941c3731bc67" id="wp-i18n-js"></script>
+<script src="https://suvir1408.github.io/test2/wp-includes/js/dist/hooks.min.js?ver=50e23bed88bcb9e6e14023e9961698c1" id="wp-hooks-js"></script>
+<script src="https://suvir1408.github.io/test2/wp-includes/js/dist/i18n.min.js?ver=db9a9a37da262883343e941c3731bc67" id="wp-i18n-js"></script>
 <script id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
@@ -78,14 +78,14 @@ var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2021-03-05 12:20:18+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"en_GB"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
 </script>
-<script src="https://suvir1408.github.io/huy1408/wp-admin/js/password-strength-meter.min.js?ver=5.7.2" id="password-strength-meter-js"></script>
-<script src="https://suvir1408.github.io/huy1408/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
+<script src="https://suvir1408.github.io/test2/wp-admin/js/password-strength-meter.min.js?ver=5.7.2" id="password-strength-meter-js"></script>
+<script src="https://suvir1408.github.io/test2/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
 <script id="wp-util-js-extra">
 var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-ajax.php"}};
 </script>
-<script src="https://suvir1408.github.io/huy1408/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
+<script src="https://suvir1408.github.io/test2/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"c035161ac3"};
+var userProfileL10n = {"user_id":"0","nonce":"88e6b0bb28"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
@@ -94,7 +94,7 @@ var userProfileL10n = {"user_id":"0","nonce":"c035161ac3"};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2021-03-05 12:20:18+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"en_GB"},"Your new password has not been saved.":["Your new password has not been saved."],"Show":["Show"],"Hide":["Hide"],"Confirm use of weak password":["Confirm use of weak password"],"Hide password":["Hide password"],"Show password":["Show password"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
 </script>
-<script src="https://suvir1408.github.io/huy1408/wp-admin/js/user-profile.min.js?ver=5.7.2" id="user-profile-js"></script>
+<script src="https://suvir1408.github.io/test2/wp-admin/js/user-profile.min.js?ver=5.7.2" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
